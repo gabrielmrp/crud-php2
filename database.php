47119ba8f531c64a7a7f3e515b94a0ced453a631
Gabriel Mascarenhas <gabrielmrdp@gmail.com>
2021-03-31 01:16:49 -0300

    Update database.php

--- database.php
+++ database.php
@@ -10,7 +10,7 @@ $is_development = @file_get_contents('development')!==false;
 $database = !$is_development?'db_a71d65_sql1039':'phpcrud';
 $username = !$is_development?'a71d65_sql1039':'root';
 $password = !$is_development?'trinta30':'root';
-$host =     !$is_development?'mysql5030.site4now.net':'db';
+$host =     !$is_development?'MYSQL5030.site4now.net':'db';
 
 $capsule->addConnection([
     'driver'    => 'mysql',
